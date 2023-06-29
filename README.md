45eb551764e5306c876ad17413cb7cbedf7773db
Gustavo de Paula <gdepaula@gmail.com>
2023-06-29 17:50:13 -0300

    Create Movie Component

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 - Movie info
 
 API stuff
-featured: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key="
-images: "https://image.themoviedb.org/t/p/w1280"
-search: "https://api.themoviedb.org/3/discover/movie?&api_key=&query"
+
+- featured: "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3a66a6524b8bd8e2a826a73f5535335d"
+- images: "https://image.themoviedb.org/t/p/w1280"
+- search: "https://api.themoviedb.org/3/discover/movie?&api_key=3a66a6524b8bd8e2a826a73f5535335d&query"
--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
+
+const FEATURED_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3a66a6524b8bd8e2a826a73f5535335d&page=1";
+
+const SEARCH_API =
+  "https://api.themoviedb.org/3/discover/movie?&api_key=3a66a6524b8bd8e2a826a73f5535335d&query=";
+
 function App() {
-  return <div>Hello Popers</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(FEATURED_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setMovies(data.results);
+      });
+    // const moviesResp = await fetch(FEATURED_API);
+    // const moviesR = await moviesResp.json();
+  }, []);
+  return (
+    <div>
+      {movies.length > 0 &&
+        movies.map((movie) => <Movie key={movie.id} {...movie} />)}
+    </div>
+  );
 }
 
 export default App;
