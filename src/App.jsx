71789bc332b9b2c6a47168bcc16eeb270889d87c
Gustavo de Paula <gdepaula@gmail.com>
2023-07-11 15:03:29 -0300

    Cleaning Up Data Fetching

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,27 @@ function App() {
   const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
-    getMovies(FEATURED_API);
+    const controller = new AbortController();
+
+    getMovies(FEATURED_API, { signal: controller.signal });
+
+    return function () {
+      controller.abort();
+    };
   }, []);
 
-  async function getMovies(API) {
+  async function getMovies(API, SIGNAL) {
     try {
-      const res = await fetch(API);
+      const res = await fetch(API, SIGNAL);
       if (!res.ok) throw new Error("Something went wrong with fetching movies");
 
       const data = await res.json();
 
       setMovies(data.results);
     } catch (err) {
-      console.error(err.message);
+      if (err.name !== "AbortError") {
+        console.error(err.message);
+      }
     }
   }
 
