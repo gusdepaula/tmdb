ebb6b4d9bd5e80fc6bb4c81851e11d99cc10cdb4
Gustavo de Paula <gdepaula@gmail.com>
2023-08-02 11:34:38 -0300

    Including a Spinner

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import NewMovie from "./pages/NewMovie";
+import SpinnerFullPage from "./components/Spinner/SpinnerFullPage";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="movie/:id" element={<NewMovie />} />
-      </Routes>
+      <Suspense fallback={<SpinnerFullPage />}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="movie/:id" element={<NewMovie />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 );
