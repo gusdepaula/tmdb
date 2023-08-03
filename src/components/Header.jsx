9cef546bb6ff8adcbd2cf333fed329b0359fc0fd
Gustavo de Paula <gdepaula@gmail.com>
2023-08-03 14:40:18 -0300

    Including Logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,33 @@ import { AiOutlineSearch } from "react-icons/ai";
 const Header = styled.header`
   background-color: #373b69;
   display: flex;
+  align-items: center;
   padding: 1rem;
-  justify-content: flex-end;
+  justify-content: space-between; /* Novo: alinhar o h1 à esquerda e o input search à direita */
+
+  @media (max-width: 768px) {
+    flex-direction: column; /* Novo: em telas menores, colocar os elementos em coluna */
+    align-items: flex-start; /* Novo: alinhar os elementos à esquerda */
+  }
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  font-family: "Roboto", sans-serif; /* Nova fonte */
+  color: #fff; /* Cor do texto */
+  margin: 0;
+  padding-right: 1rem;
+  display: flex;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    margin-bottom: 1rem;
+  }
+`;
+
+const Logo = styled.span`
+  font-size: 2.5rem; /* Tamanho do logo */
+  margin-right: 0.5rem; /* Espaçamento entre o logo e o texto */
 `;
 
 const SearchContainer = styled.div`
@@ -45,19 +70,22 @@ const SearchButton = styled.button`
   padding: 5px;
   margin-left: 5px;
   cursor: pointer;
-  position: absolute;
-  right: 33px;
   font-size: 1.5rem;
+  color: #fff; /* Novo: cor do ícone de pesquisa */
   transition: color 0.3s;
 
   &:hover {
-    color: #fff;
+    color: #f1f1f1; /* Novo: cor do ícone de pesquisa no hover */
   }
 `;
 
 function HeaderWithSearch({ searchTerm, handleOnSubmit, handleOnChange }) {
   return (
     <Header>
+      <Title>
+        <Logo>🎞</Logo>
+        TMdB
+      </Title>
       <form onSubmit={handleOnSubmit}>
         <SearchContainer>
           <Search
